0d843a37a73f6bd3f18f42215b9821fc0acab525
Lalit Magar <lalitmagar1729@gmail.com>
2026-01-08 15:52:04 +0530

    Update MySQL connection details

--- conn.js
+++ conn.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "a2z47_ecom"
+    host: "by8yxirheybwrdflhznk-mysql.services.clever-cloud.com",
+    user: "udfge0vm4mzzjqxl",
+    password: "YZzlokTZmg0pgYlKIw5u",
+    database: "by8yxirheybwrdflhznk"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
