f1bc60728de71c2f96176c21167c6c377fbce812
Sakshi Mohite <sakshimohite206@gmail.com>
2026-01-09 11:10:26 +0530

    Update MySQL connection credentials

--- conn.js
+++ conn.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host: "by8yxirheybwrdflhznk-mysql.services.clever-cloud.com",
-    user: "udfge0vm4mzzjqxl",
-    password: "YZzlokTZmg0pgYlKIw5u",
-    database: "by8yxirheybwrdflhznk"
+    host: "bhczgmwkb7cct49uitrt-mysql.services.clever-cloud.com",
+    user: "uwyeq2otxgtoip2e",
+    password: "ljYTH7uPW5x9zvkm3TZW",
+    database: "bhczgmwkb7cct49uitrt"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
